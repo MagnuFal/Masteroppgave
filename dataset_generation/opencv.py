832446ff97b3c768ce60e4ec6a1e8a30ea0f81a4
Magnus S. Fallmyr <magnufal@stud.ntnu.no>
2026-01-27 18:14:03 +0100

    data generation w/ opencv

--- dataset_generation/opencv.py
+++ dataset_generation/opencv.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2 as cv
+from PIL import Image
 
 def blobs(img, x, y, r):
     x2 = np.random.randint(x - 200, x + 200)
@@ -22,27 +23,38 @@ def blobs(img, x, y, r):
 
 width, height = 2560, 1790
 
-img = np.zeros((height, width, 3), dtype=np.uint8)
+def generate_images(n_images):
+    for k in range(n_images):
+        img = np.zeros((height, width, 3), dtype=np.uint8)
 
-n_blobs = np.random.randint(1, 10)
-x_circle = np.random.randint(0, width, size=n_blobs)
-y_circle = np.random.randint(0, height, size=n_blobs)
-r_circle = np.random.randint(100, 200, size=n_blobs)
+        n_blobs = np.random.randint(1, 10)
+        x_circle = np.random.randint(0, width, size=n_blobs)
+        y_circle = np.random.randint(0, height, size=n_blobs)
+        r_circle = np.random.randint(100, 200, size=n_blobs)
 
-n_plates = np.random.randint(10, 30)
-x_plates = np.random.randint(0, width, size=n_plates)
-y_plates = np.random.randint(0, height, size=n_plates)
-major_axis_lenght = np.random.randint(100, 1500, size=n_plates)
-minor_axis_lenght = np.random.randint(100, 1500, size=n_plates)
-angle = np.random.randint(0, 360, size=n_plates)
-start_angle = np.random.randint(0, 360, size=(n_plates))
-end_angle = np.random.randint(start_angle, start_angle + 3, size=n_plates)
+        n_plates = np.random.randint(10, 30)
+        x_plates = np.random.randint(0, width, size=n_plates)
+        y_plates = np.random.randint(0, height, size=n_plates)
+        major_axis_lenght = np.random.randint(100, 1500, size=n_plates)
+        minor_axis_lenght = np.random.randint(100, 1500, size=n_plates)
+        angle = np.random.randint(0, 360, size=n_plates)
+        start_angle = np.random.randint(0, 360, size=(n_plates))
+        end_angle = np.random.randint(start_angle, start_angle + 3, size=n_plates)
 
-for i in range(n_blobs):
-    blobs(img, x_circle[i], y_circle[i], r_circle[i])
+        for j in range(n_plates):
+            cv.ellipse(img, (x_plates[j], y_plates[j]), (major_axis_lenght[j], minor_axis_lenght[j]), angle[j], start_angle[j], end_angle[j], (0, 0, 255), -1)
 
-fig, ax = plt.subplots(1, 1)
+        for i in range(n_blobs):
+            blobs(img, x_circle[i], y_circle[i], r_circle[i])
 
-ax.imshow(img)
+        im = Image.fromarray(img)
 
-plt.show()
+        im.save(rf"C:\Users\magfa\Documents\Master\Masteroppgave\synthetic_dataset\label\{k}.png")
+
+        raw = img.mean(axis=2).astype(np.uint8) * 255
+
+        im2 = Image.fromarray(raw)
+
+        im2.save(rf"C:\Users\magfa\Documents\Master\Masteroppgave\synthetic_dataset\raw\{k}.png")
+
+generate_images(100)
