9e3892c304dbb97634b930ca18e1d4ad36660cf6
Magnus S. Fallmyr <magnufal@stud.ntnu.no>
2026-01-29 16:03:49 +0100

    first model draft

--- machine_learning/model.py
+++ machine_learning/model.py
@@ -24,8 +24,8 @@ class DownPass(nn.Module):
     
         self.maxpool = nn.MaxPool2d(kernel_size=2)
         self.downpass = nn.Sequential(
-            DoubleConvolution(in_channels=in_channels, out_channels=out_channels),
-            self.maxpool()
+            self.maxpool(),
+            DoubleConvolution(in_channels=in_channels, out_channels=out_channels)
         )
 
     def forward(self, x):
@@ -39,7 +39,26 @@ class UpPass(nn.Module):
         self.double = DoubleConvolution(in_channels=in_channels, out_channels=out_channels)
 
     def forward(self, x1, x2):
-        self.up
+        self.up(x1)
+
+        dy = x2.size()[2] - x1.size()[2]
+        dx = x2.size()[3] - x1.size()[3]
+
+        x1_pad = F.pad(x1, [dx // 2, dx - dx // 2, dy // 2, dy - dy // 2])
+
+        x_cat = torch.cat([x2, x1_pad], dim = 1)
+
+        return self.double(x_cat)
+    
+class FinalConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=1)
+
+    def forward(self, x):
+        self.conv(x)
+
 
 class UNet(nn.Module):
     def __init__(self, n_input_channels = 1, n_classes = 3):
@@ -50,3 +69,22 @@ class UNet(nn.Module):
         self.down_pass2 = DownPass(128, 256)
         self.down_pass3 = DownPass(256, 512)
         self.down_pass4 = DownPass(512, 1024)
+        self.up_pass1 = UpPass(1024, 512)
+        self.up_pass2 = UpPass(512, 256)
+        self.up_pass3 = UpPass(256, 128)
+        self.up_pass4 = UpPass(128, 64)
+        self.double_conv2 = FinalConv(64, n_classes)
+
+    def forward(self, x):
+        x1 = self.double_conv1(x)
+        x2 = self.down_pass1(x1)
+        x3 = self.down_pass1(x2)
+        x4 = self.down_pass1(x3)
+        x5 = self.down_pass1(x4)
+        x6 = self.up_pass1(x5, x4)
+        x7 = self.up_pass1(x6, x3)
+        x8 = self.up_pass1(x7, x2)
+        x9 = self.up_pass1(x8, x1)
+        x10 = self.double_conv2(x9)
+
+        return x10
