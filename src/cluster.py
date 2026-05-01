35cef0cfba896e729139c3211733366e947921fe
Magnus S. Fallmyr <magnufal@stud.ntnu.no>
2026-05-01 11:50:41 +0200

    inference on training set of phase extraction

--- src/cluster.py
+++ src/cluster.py
@@ -10,47 +10,49 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 if __name__ == "__main__":
     
-    raw_dir = r"/cluster/home/magnufal/Master/Masteroppgave/data/two step model phase extraction/train/raw"
-    label_dir = r"/cluster/home/magnufal/Master/Masteroppgave/data/two step model phase extraction/train/label"
-
-    #raw_dir = r"C:\Users\magfa\Documents\Master\Masteroppgave\data\sanity_test\raw"
-    #label_dir = r"C:\Users\magfa\Documents\Master\Masteroppgave\data\sanity_test\label"
-
-    dataset = SyntheticDatasetAugmented(raw_dir, label_dir)
-
-    val_percent = 0.1
-
-    n_val = int(len(dataset) * val_percent)
-    n_train = len(dataset) - n_val
-
-    train_set, val_set = random_split(dataset, [n_train, n_val])
-
-    train_loader = DataLoader(train_set, shuffle=True, batch_size=1)
-    val_loader = DataLoader(val_set, shuffle=False, batch_size=1)
+    #raw_dir = r"/cluster/home/magnufal/Master/Masteroppgave/data/two step model phase extraction/train/raw"
+    #label_dir = r"/cluster/home/magnufal/Master/Masteroppgave/data/two step model phase extraction/train/label"
 #
-    training_weights = torch.tensor([0.346777, 8.598215])
-#
-    model = UNet(n_classes=2)
-    sv_pt = r"/cluster/home/magnufal/Master/Masteroppgave/machine_learning/two_step_model_phase_extraction_run_1.pth"
-    #sv_pt = r"C:\Users\magfa\Documents\Master\Masteroppgave\machine_learning/sanity_test.pth"
-    #checkpoint = torch.load(r"/cluster/home/magnufal/Master/Masteroppgave/machine_learning/re_test_improved_dataset_2_with_train_val_loss_15_04_26.pth", weights_only=True, map_location=torch.device(device))
-    #model.load_state_dict(checkpoint['model_state_dict'])
-    optimization_loop(model, save_path=sv_pt, tr_loader=train_loader, vl_loader=val_loader, weights=training_weights, epochs= 300)
-
-    # --------------- Test ----------------------
-
-    #raw_dir = r"C:\Users\magfa\Documents\Master\Masteroppgave\data\dataset_3_improved\test\raw"
-    #label_dir = r"C:\Users\magfa\Documents\Master\Masteroppgave\data\dataset_3_improved\test\label"
-#
+    ##raw_dir = r"C:\Users\magfa\Documents\Master\Masteroppgave\data\sanity_test\raw"
+    ##label_dir = r"C:\Users\magfa\Documents\Master\Masteroppgave\data\sanity_test\label"
 #
     #dataset = SyntheticDatasetAugmented(raw_dir, label_dir)
 #
-    #test_loader = DataLoader(dataset, shuffle=False)
+    #val_percent = 0.1
 #
-    #model = UNet()
-    #checkpoint = torch.load(r"C:\Users\magfa\Documents\Master\Masteroppgave\experiments\dataset_3\dataset_3_improved_7th_run_AdamW\dataset_3_improved_7th_run_AdamW.pth", weights_only=True, map_location=torch.device('cpu'))
-    #model.load_state_dict(checkpoint['model_state_dict'])
+    #n_val = int(len(dataset) * val_percent)
+    #n_train = len(dataset) - n_val
 #
-    #model_test(model, test_loader, save_folder_path = r"C:\Users\magfa\Documents\Master\Masteroppgave\experiments\dataset_3\dataset_3_improved_7th_run_AdamW\predictions")
+    #train_set, val_set = random_split(dataset, [n_train, n_val])
+#
+    #train_loader = DataLoader(train_set, shuffle=True, batch_size=1)
+    #val_loader = DataLoader(val_set, shuffle=False, batch_size=1)
+##
+    #training_weights = torch.tensor([0.346777, 8.598215])
+##
+    #model = UNet(n_classes=2)
+    #sv_pt = r"/cluster/home/magnufal/Master/Masteroppgave/machine_learning/two_step_model_phase_extraction_run_1.pth"
+    ##sv_pt = r"C:\Users\magfa\Documents\Master\Masteroppgave\machine_learning/sanity_test.pth"
+    ##checkpoint = torch.load(r"/cluster/home/magnufal/Master/Masteroppgave/machine_learning/re_test_improved_dataset_2_with_train_val_loss_15_04_26.pth", weights_only=True, map_location=torch.device(device))
+    ##model.load_state_dict(checkpoint['model_state_dict'])
+    #optimization_loop(model, save_path=sv_pt, tr_loader=train_loader, vl_loader=val_loader, weights=training_weights, epochs= 300)
+
+    # --------------- Test ----------------------
+
+    #raw_dir = r"C:\Users\magfa\Documents\Master\Masteroppgave\data\two-step model datasets\step 1 phase extraction\test\raw"
+    #label_dir = r"C:\Users\magfa\Documents\Master\Masteroppgave\data\two-step model datasets\step 1 phase extraction\test\label"
+
+    raw_dir = r"/cluster/home/magnufal/Master/Masteroppgave/data/two step model phase differentiation/train/raw"
+    label_dir = r"/cluster/home/magnufal/Master/Masteroppgave/data/two step model phase differentiation/train/label"
+
+    dataset = SyntheticDatasetAugmented(raw_dir, label_dir)
+
+    test_loader = DataLoader(dataset, shuffle=False)
+
+    model = UNet(n_classes=2)
+    checkpoint = torch.load(r"C:\Users\magfa\Documents\Master\Masteroppgave\experiments\two-step model\phase extraction run 1\two_step_model_phase_extraction_run_1.pth", weights_only=True, map_location=torch.device('cpu'))
+    model.load_state_dict(checkpoint['model_state_dict'])
+
+    model_test(model, test_loader, save_folder_path = r"/cluster/home/magnufal/Master/Masteroppgave/experiments/predictions on training set of phase extraction")
     #model = UNet()
     #summary(model, (1, 1, 224, 224))
--- src/rgb_prediction_to_8_bit.py
+++ src/rgb_prediction_to_8_bit.py
@@ -8,7 +8,7 @@ def rgb_to_8_bit(three_channel_tensor, axis = 2):
     
     return np.argmax(pred_array, axis=axis)
 
-def predictions_argmax_from_folder(folder_path, save_folder_path1, save_folder_path2):
+def predictions_argmax_from_folder_rgb(folder_path, save_folder_path1, save_folder_path2):
     folder = Path(folder_path)
     for file in folder.iterdir():
         img = Image.open(file)
@@ -20,8 +20,21 @@ def predictions_argmax_from_folder(folder_path, save_folder_path1, save_folder_p
         argmax.save(f"{save_folder_path1}\{file.stem}.png")
         argmax_v.save(f"{save_folder_path2}\{file.stem}.png")
 
+def predictions_argmax_from_folder_binary(folder_path, save_folder_path1, save_folder_path2):
+    folder = Path(folder_path)
+    for file in folder.iterdir():
+        img = Image.open(file)
+        arr = np.asarray(img)
+        eight_bit = rgb_to_8_bit(arr).astype(np.uint8)
+        argmax = Image.fromarray(eight_bit)
+        rgb = (eight_bit > 0).astype(int)
+        rgb = (rgb * 255).astype(np.uint8)
+        argmax_v = Image.fromarray(rgb)
+        argmax.save(f"{save_folder_path1}\{file.stem}.png")
+        argmax_v.save(f"{save_folder_path2}\{file.stem}.png")
+
 if __name__ == "__main__":
-    predictions_argmax_from_folder(r"C:\Users\magfa\Documents\Master\Masteroppgave\experiments\dataset_3\dataset_3_improved_7th_run_AdamW\predictions",
-                                   r"C:\Users\magfa\Documents\Master\Masteroppgave\experiments\dataset_3\dataset_3_improved_7th_run_AdamW\predictions_argmax",
-                                   r"C:\Users\magfa\Documents\Master\Masteroppgave\experiments\dataset_3\dataset_3_improved_7th_run_AdamW\predictions_argmax_v")
+    predictions_argmax_from_folder_binary(r"C:\Users\magfa\Documents\Master\Masteroppgave\experiments\two-step model\phase extraction run 1\predictions",
+                                   r"C:\Users\magfa\Documents\Master\Masteroppgave\experiments\two-step model\phase extraction run 1\predictions_argmax",
+                                   r"C:\Users\magfa\Documents\Master\Masteroppgave\experiments\two-step model\phase extraction run 1\predictions_argmax_v")
         
